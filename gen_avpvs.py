afbc717d2c4cf4da04bf57c3c8851a87eaa36e74
Steve <stg7@gmx.de>
2019-10-31 00:30:32 +0100

    minimal changes, that dont affect the function hopefully

--- gen_avpvs.py
+++ gen_avpvs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-
+"""
+script to convert a given video sequence to
+a avpvs version (rescaled to the same resolution than the source video including framerate adaption)
+"""
 import os
 import sys
 import argparse
-import glob
-import pandas as pd
 import subprocess as sp
 import shlex
-import pprint
 import json
 
-import yaml
+
 
 def get_fps(src_video):
+    """
+    returns framerate of a given video `src_video`
+    """
     ffprobe_cmd = "ffprobe -v quiet -print_format json -show_streams"
     args = shlex.split(ffprobe_cmd)
     args.append(src_video)
@@ -20,16 +23,21 @@ def get_fps(src_video):
     ffprobeOutput = json.loads(ffprobeOutput)
 
     fps = ffprobeOutput["streams"][0]["avg_frame_rate"]
-    nr = int(fps.split("/")[0])
-    dr = int(fps.split("/")[1])
-    framerate = round((nr/dr),2)
-    return framerate
+
+    if "/" in fps:
+        parts = fps.split("/")
+        nr = int(parts[0])
+        dr = int(parts[1])
+        return round(nr / dr, 2)
+
+    return round(float(fps), 2)
+
 
 def avpvs_gen(input_file, src_video, avpvs_folder):
     avpvs_width = 3840
     avpvs_height = 2160
     output_file = os.path.join(avpvs_folder, os.path.splitext(os.path.basename(input_file))[0] + "_avpvs.mkv")
-    print(output_file)
+    print(f"create {output_file} based on {input_file}")
     target_pix_fmt = "yuv422p10le"
 
     # ffprobe_cmd = """ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=r_frame_rate {src_video}""".format(**locals())
@@ -38,12 +46,23 @@ def avpvs_gen(input_file, src_video, avpvs_folder):
     src_framerate = get_fps(src_video)
     print(src_framerate)
 
-    cmd = """ffmpeg -nostdin -i {input_file} -filter:v scale={avpvs_width}:{avpvs_height}:flags=bicubic,fps={src_framerate},setsar=1/1 -c:v ffvhuff -threads 4 -level 3 -coder 1 -context 1 -slicecrc 1 -pix_fmt {target_pix_fmt} -an {output_file}""".format(**locals())
+    cmd = " ".join(f"""
+    ffmpeg -nostdin -i "{input_file}"
+    -filter:v scale={avpvs_width}:{avpvs_height}:flags=bicubic,fps={src_framerate},setsar=1/1
+    -c:v ffvhuff
+    -threads 4
+    -level 3
+    -coder 1
+    -context 1
+    -slicecrc 1
+    -pix_fmt {target_pix_fmt}
+    -an "{output_file}" """.split("\n"))
 
     ret = os.system(cmd)
     if ret != 0:
         sys.exit(-1)
     return output_file
+
 
 def main(_):
     # argument parsing
@@ -57,6 +76,7 @@ def main(_):
     avpvs_gen(args["videosegment"], args["src"], args["avpvs_folder"])
 
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
